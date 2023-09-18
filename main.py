e90571f3c7f444641fbe970039df0115f6696264
GabrielaBPorto <gabriela.bairros.porto96@gmail.com>
2023-09-18 13:04:51 -0300

    feature: open imagem; feature: transform to monocromatic image

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import sys
 import re
+import cv2
 
 def check_image_extension(filename):
     return bool(re.search(r'\.(png|jpg|jpeg|gif|bmp)$', filename, re.IGNORECASE))
+
+def openImage(filename):
+    img = cv2.imread(filename)
+    if img is None:
+        print("Could not open or find the image.")
+        sys.exit(1)
+    return img
+
+def display_image(img):
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def param_checking(filename,percentage,technique):
+    if not check_image_extension(filename):
+        print("You must provide an image.")
+        sys.exit(1)
+    if percentage <= 0 or percentage >=100:
+        print("You must provide a valid reduction percentage.")
+        sys.exit(1)
+    if technique not in ['media','moda','mediana']:
+        print("You must provide a valid reduction technique.")
+        sys.exit(1)
 
 def main(imageName, reductionPercentage, technique):
     print(f"Parameter 1: {imageName}")
     print(f"Parameter 2: {reductionPercentage}")
     print(f"Parameter 3: {technique}")
+    img = openImage(imageName)
+
+    # Caso a imagem já não esteja em um tom monocromático agora ela se torna mono
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    display_image(img)
+    display_image(image)
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -15,16 +45,7 @@ if __name__ == "__main__":
         sys.exit(1)
     
     imageName = sys.argv[1]
-    if not check_image_extension(imageName):
-        print("You must provide an image.")
-        sys.exit(1)
     reductionPercentage = int(sys.argv[2])
-    if reductionPercentage <= 0 or reductionPercentage >=100:
-        print("You must provide a valid reduction percentage.")
-        sys.exit(1)
     technique = sys.argv[3]
-    if technique not in ['media','moda','mediana']:
-        print("You must provide a valid reduction technique.")
-        sys.exit(1)
-    
+    param_checking(imageName, reductionPercentage, technique)
     main(imageName, reductionPercentage, technique)
